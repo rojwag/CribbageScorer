a03b146ba831a0a5bd57385fc73eb82010323409
Rojay Wagner <rojwag@gmail.com>
2017-07-01 11:43:38 -0400

    Initial project commit

---
+++ CribbageScorer/Scorer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CribbageScorer
+{
+    public class Scorer
+    {
+        public Scorer()
+        {
+            Console.WriteLine("This is a scorer");
+            Console.ReadKey();
+        }
+    }
+}

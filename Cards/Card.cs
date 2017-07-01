8b130d9080aaafbba2c41a39b89d80f8ccc46f74
Rojay Wagner <rojwag@gmail.com>
2017-07-01 11:51:50 -0400

    Adding Deck and Cards

---
+++ Cards/Card.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Suit = Cards.Deck.Suit;
+using Value = Cards.Deck.Value;
+
+namespace Cards
+{
+    public class Card
+    {
+        public Suit Suit
+        {
+            get; private set;
+        }
+
+        public Value Value
+        {
+            get; private set;
+        }
+
+        public int Number
+        {
+            get { return (int)Value + 1; }
+        }
+
+        public int Number10
+        {
+            get { return Number > 10 ? 10 : Number; }
+        }
+
+        // TODO: make internal
+        public Card(Suit suit, Value value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} of {1}", Value, Suit);
+        }
+
+        public string ToStringShort()
+        {
+            string valueString = string.Empty;
+
+            switch (Number)
+            {
+                case 1: valueString = "A"; break;
+                case 11: valueString = "J"; break;
+                case 12: valueString = "Q"; break;
+                case 13: valueString = "K"; break;
+                default: valueString = Number.ToString(); break;
+            }
+
+            valueString += Suit.ToString().Substring(0, 1);
+
+            return valueString;
+        }
+
+        public bool Equals(Card c)
+        {
+            return this.Suit == c.Suit && this.Value == c.Value;
+        }
+    }
+}
---
+++ Cards/Deck.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cards
+{
+    public class Deck
+    {
+        public List<Card> Cards
+        {
+            get; private set;
+        }
+
+        public Deck(int numDecks)
+        {
+            Cards = CreateNewDeck(numDecks);
+        }
+
+        public void Shuffle()
+        {
+            List<Card> shuffledCards = new List<Card>();
+            Random rand = new Random();
+
+            while (Cards.Count > 0)
+            {
+                int index = rand.Next(0, Cards.Count - 1);
+                shuffledCards.Add(Cards[index]);
+                Cards.RemoveAt(index);
+            }
+
+            Cards = shuffledCards;
+        }
+
+        private List<Card> CreateNewDeck(int numDecks)
+        {
+            List<Card> deck = new List<Card>();
+
+            for (int deckNum = 0; deckNum < numDecks; deckNum++)
+            {
+                foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+                {
+                    foreach (Value value in Enum.GetValues(typeof(Value)))
+                    {
+                        deck.Add(new Card(suit, value));
+                    }
+                }
+            }
+
+            return deck;
+        }
+
+        public enum Suit
+        {
+            Spades, Hearts, Diamonds, Clubs
+        }
+
+        public enum Value
+        {
+            Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+        }
+    }
+}

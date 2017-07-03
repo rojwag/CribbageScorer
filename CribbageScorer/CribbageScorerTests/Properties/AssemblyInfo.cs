a0cb7c9132c5cc1caa041f1f25eae5f96fd4da1d
Rojay Wagner <rojwag@gmail.com>
2017-07-03 07:47:26 -0400

    Adding unit tests project and basic tests

---
+++ CribbageScorer/CribbageScorerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CribbageScorerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CribbageScorerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("506eecc9-23e4-4910-a0f7-e2b523aa4a67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CribbageScorer/CribbageScorerTests/TestScores.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Card = Cards.Card;
+using Suit = Cards.Deck.Suit;
+using Value = Cards.Deck.Value;
+
+namespace CribbageScorerTests
+{
+    [TestClass]
+    public class TestScores
+    {
+        CribbageScorer.Scorer Scorer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            Scorer = new CribbageScorer.Scorer();
+        }
+
+        [TestMethod]
+        public void Fifteens()
+        {
+            List<Card> hand = new List<Card>
+            {
+                new Card(Suit.Clubs, Value.Five),
+                new Card(Suit.Spades, Value.Four),
+                new Card(Suit.Clubs, Value.Ten),
+                new Card(Suit.Clubs, Value.Jack)
+            };
+            Card crib = new Card(Suit.Diamonds, Value.Ace);
+
+            Assert.AreEqual(Scorer.Score(hand, crib), 4);
+        }
+
+        [TestMethod]
+        public void RunOf3()
+        {
+            List<Card> hand = new List<Card>
+            {
+                new Card(Suit.Clubs, Value.Eight),
+                new Card(Suit.Spades, Value.Nine),
+                new Card(Suit.Clubs, Value.Jack),
+                new Card(Suit.Clubs, Value.Queen)
+            };
+            Card crib = new Card(Suit.Diamonds, Value.King);
+
+            Assert.AreEqual(Scorer.Score(hand, crib), 3);
+        }
+
+        [TestMethod]
+        public void RunOf4()
+        {
+            List<Card> hand = new List<Card>
+            {
+                new Card(Suit.Clubs, Value.Eight),
+                new Card(Suit.Spades, Value.Ten),
+                new Card(Suit.Clubs, Value.Jack),
+                new Card(Suit.Clubs, Value.Queen)
+            };
+            Card crib = new Card(Suit.Diamonds, Value.King);
+
+            Assert.AreEqual(Scorer.Score(hand, crib), 4);
+        }
+
+        [TestMethod]
+        public void RunOf5()
+        {
+            List<Card> hand = new List<Card>
+            {
+                new Card(Suit.Clubs, Value.Nine),
+                new Card(Suit.Spades, Value.Ten),
+                new Card(Suit.Clubs, Value.Jack),
+                new Card(Suit.Clubs, Value.Queen)
+            };
+            Card crib = new Card(Suit.Diamonds, Value.King);
+
+            Assert.AreEqual(Scorer.Score(hand, crib), 5);
+        }
+
+        [TestMethod]
+        public void DoubleRun()
+        {
+            List<Card> hand = new List<Card>
+            {
+                new Card(Suit.Clubs, Value.Nine),
+                new Card(Suit.Spades, Value.Ten),
+                new Card(Suit.Clubs, Value.Ten),
+                new Card(Suit.Clubs, Value.Jack)
+            };
+            Card crib = new Card(Suit.Diamonds, Value.King);
+
+            Assert.AreEqual(Scorer.Score(hand, crib), 8);
+        }
+
+        [TestMethod]
+        public void TripleRun()
+        {
+            List<Card> hand = new List<Card>
+            {
+                new Card(Suit.Clubs, Value.Nine),
+                new Card(Suit.Spades, Value.Ten),
+                new Card(Suit.Clubs, Value.Ten),
+                new Card(Suit.Clubs, Value.Ten)
+            };
+            Card crib = new Card(Suit.Diamonds, Value.Jack);
+
+            Assert.AreEqual(Scorer.Score(hand, crib), 15);
+        }
+
+        [TestMethod]
+        public void DoubleDoubleRun()
+        {
+            List<Card> hand = new List<Card>
+            {
+                new Card(Suit.Clubs, Value.Nine),
+                new Card(Suit.Spades, Value.Nine),
+                new Card(Suit.Clubs, Value.Ten),
+                new Card(Suit.Clubs, Value.Ten)
+            };
+            Card crib = new Card(Suit.Diamonds, Value.Jack);
+
+            Assert.AreEqual(Scorer.Score(hand, crib), 16);
+        }
+
+        [TestMethod]
+        public void DoubleRunOf4()
+        {
+            List<Card> hand = new List<Card>
+            {
+                new Card(Suit.Clubs, Value.Nine),
+                new Card(Suit.Spades, Value.Nine),
+                new Card(Suit.Clubs, Value.Ten),
+                new Card(Suit.Clubs, Value.Jack)
+            };
+            Card crib = new Card(Suit.Diamonds, Value.Queen);
+
+            Assert.AreEqual(Scorer.Score(hand, crib), 16);
+        }
+
+        [TestMethod]
+        public void PartialFlush()
+        {
+            List<Card> hand = new List<Card>
+            {
+                new Card(Suit.Clubs, Value.Two),
+                new Card(Suit.Clubs, Value.Four),
+                new Card(Suit.Clubs, Value.Six),
+                new Card(Suit.Clubs, Value.Eight)
+            };
+            Card crib = new Card(Suit.Diamonds, Value.Ten);
+
+            Assert.AreEqual(Scorer.Score(hand, crib), 4);
+        }
+
+        [TestMethod]
+        public void FullFlush()
+        {
+            List<Card> hand = new List<Card>
+            {
+                new Card(Suit.Clubs, Value.Two),
+                new Card(Suit.Clubs, Value.Four),
+                new Card(Suit.Clubs, Value.Six),
+                new Card(Suit.Clubs, Value.Eight)
+            };
+            Card crib = new Card(Suit.Clubs, Value.Ten);
+
+            Assert.AreEqual(Scorer.Score(hand, crib), 5);
+        }
+
+        [TestMethod]
+        public void RightJack()
+        {
+            List<Card> hand = new List<Card>
+            {
+                new Card(Suit.Clubs, Value.Two),
+                new Card(Suit.Clubs, Value.Four),
+                new Card(Suit.Clubs, Value.Six),
+                new Card(Suit.Diamonds, Value.Jack)
+            };
+            Card crib = new Card(Suit.Diamonds, Value.Ten);
+
+            Assert.AreEqual(Scorer.Score(hand, crib), 1);
+        }
+    }
+}
